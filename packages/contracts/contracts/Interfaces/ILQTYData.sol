2158731e6c1c4919ca2404ec77718d0dc9af6d3b
a <b>
2023-04-30 23:06:43 +0200

    Add data contracts

---
+++ packages/contracts/contracts/Interfaces/ILQTYData.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.11;
+
+import "../Dependencies/IERC20.sol";
+import "../Dependencies/IERC2612.sol";
+
+interface ILQTYData {
+    // --- Functions ---
+    function getDeploymentStartTime() external view returns (uint256);
+
+    function getLpRewardsEntitlement() external view returns (uint256);
+}
